86ed764e191e2883b1fafbaae7131329ee5ece16
V-R-DhanushRaj <vrdhanushraj@gmail.com>
2025-05-02 12:55:32 +0530

    added search and add passwd button

--- Resource/gui.py
+++ Resource/gui.py
@@ -5,19 +5,24 @@ import settings
 class GUI(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.app_mode = settings.appearance_mode
-        ctk.set_appearance_mode(self.app_mode)
+        self.APP_MODE = settings.appearance_mode
+        ctk.set_appearance_mode(self.APP_MODE)
 
         # Images
         self.dashboard_img = ctk.CTkImage(light_image=Image.open("../Images/dashboard_light.png"),
                                           dark_image=Image.open('../Images/dashboard_light.png'), size=(30, 30))
-        """self.my_passwd_img = ctk.CTkImage(light_image=Image.open(''), dark_image=Image.open(''), size=(30, 30))
-        self.settings_img = ctk.CTkImage(light_image=Image.open(''), dark_image=Image.open(''), size=(30, 30))
-        self.search_img = ctk.CTkImage(light_image=Image.open(''), dark_image=Image.open(''), size=(30, 30))
-        self.add_img = ctk.CTkImage(light_image=Image.open(''), dark_image=Image.open(''), size=(30, 30))"""
+        self.my_passwd_img = ctk.CTkImage(light_image=Image.open('../Images/analytics_light.png'),
+                                          dark_image=Image.open('../Images/analytics_light.png'), size=(30, 30))
+        self.settings_img = ctk.CTkImage(light_image=Image.open('../Images/settings_light.png'),
+                                          dark_image=Image.open('../Images/settings_light.png'), size=(30, 30))
+        self.search_img = ctk.CTkImage(light_image=Image.open('../Images/search.png'),
+                                          dark_image=Image.open('../Images/search.png'), size=(30, 30))
+        self.add_img = ctk.CTkImage(light_image=Image.open('../Images/add_light.png'),
+                                          dark_image=Image.open('../Images/add_light.png'), size=(25, 25))
 
         # Variables
         self.OPTION = 'dashboard'
+        self.SEARCH = ctk.StringVar(value='Search Password')
         self.colour_choose()
 
         # Window Settings
@@ -32,6 +37,13 @@ class GUI(ctk.CTk):
         self.canvas.create_line(335,0, 335,900, fill=self.HIGHLIGHT_COLOUR_1, width=3)
         self.canvas.pack(expand=True, fill='both')
 
+        self.create_sidebar()
+        self.create_search_add()
+        self.create_dashboard()
+        self.create_my_password()
+
+
+    def create_sidebar(self):
         # Side Bar Title
         self.side_bar_title = ctk.CTkLabel(master=self, text='Passwd Manager', text_color=self.FONT_COLOUR_1,
                                            font=('Inter', 24, 'bold'), bg_color=self.BG_COLOUR)
@@ -39,7 +51,9 @@ class GUI(ctk.CTk):
 
         # Side Bar Option
         self.dashboard = ctk.CTkFrame(master=self, width=235, height=40, corner_radius=10,
-                                      bg_color=self.BG_COLOUR, fg_color=self.HIGHLIGHT_COLOUR_1, )
+                                      bg_color=self.BG_COLOUR, fg_color=self.HIGHLIGHT_COLOUR_1)
+        self.dashboard_icon = ctk.CTkLabel(master=self.dashboard, text='', image=self.dashboard_img)
+        self.dashboard_icon.place(x=15, y=5)
         self.dashboard_text = ctk.CTkLabel(master=self.dashboard, text='Dashboard', text_color=self.FONT_COLOUR_1,
                                            font=('Inter', 16, 'bold'))
         self.dashboard_text.place(x=50, y=6)
@@ -47,6 +61,8 @@ class GUI(ctk.CTk):
 
         self.my_passwd = ctk.CTkFrame(master=self, width=235, height=40, corner_radius=10,
                                       bg_color=self.BG_COLOUR, fg_color=self.BG_COLOUR)
+        self.my_passwd_icon = ctk.CTkLabel(master=self.my_passwd, text='', image=self.my_passwd_img)
+        self.my_passwd_icon.place(x=15, y=5)
         self.my_passwd_text = ctk.CTkLabel(master=self.my_passwd, text='My Passwords', text_color=self.FONT_COLOUR_1,
                                            font=('Inter', 16, 'bold'))
         self.my_passwd_text.place(x=50, y=6)
@@ -54,21 +70,52 @@ class GUI(ctk.CTk):
 
         self.settings = ctk.CTkFrame(master=self, width=235, height=40, corner_radius=10,
                                      bg_color=self.BG_COLOUR, fg_color=self.BG_COLOUR)
+        self.settings_icon = ctk.CTkLabel(master=self.settings, text='', image=self.settings_img)
+        self.settings_icon.place(x=15, y=4)
         self.settings_text = ctk.CTkLabel(master=self.settings, text='Settings', text_color=self.FONT_COLOUR_1,
                                           font=('Inter', 16, 'bold'), bg_color=self.BG_COLOUR)
         self.settings_text.place(x=50, y=6)
         self.settings.place(x=17, y=650)
 
 
+    def create_search_add(self):
+        # Search
+        self.search = ctk.CTkFrame(master=self, width=720, height=52, corner_radius=28,
+                                   bg_color=self.BG_COLOUR, fg_color=self.HIGHLIGHT_COLOUR_1)
+        self.search.place(x=310, y=18)
+        self.search_entry = ctk.CTkEntry(master=self.search, textvariable=self.SEARCH, height=48, width=660,
+                                         border_width=0, fg_color=self.HIGHLIGHT_COLOUR_1)
+        self.search_entry.place(x=15, y=2)
+        self.search_icon = ctk.CTkLabel(master=self.search, text='', image=self.search_img)
+        self.search_icon.place(x=678, y=9)
+
+        # Add Passwd
+        self.add = ctk.CTkFrame(master=self, width=183, height=40, corner_radius=8,
+                                bg_color=self.BG_COLOUR, fg_color=self.BUTTON_COLOUR_1)
+        self.add_text = ctk.CTkLabel(master=self.add, text='Add Password', font=('Inter', 16, 'normal'), text_color=self.FONT_COLOUR_2)
+        self.add_text.place(x=20, y=6)
+        self.add_icon = ctk.CTkLabel(master=self.add, text='', image=self.add_img)
+        self.add_icon.place(x=140,y=6)
+        self.add.place(x=1075, y=22)
+
+
+    def create_dashboard(self):
+        pass
+
+
+    def create_my_password(self):
+        pass
+
+
     def colour_choose(self):
-        if self.app_mode == 'light':
+        if self.APP_MODE == 'light':
             self.BG_COLOUR = '#ffffff'
             self.HIGHLIGHT_COLOUR_1 = '#eeeeef'
             self.HIGHLIGHT_COLOUR_2 = '#cecece'
             self.BUTTON_COLOUR_1 = '#000000'
             self.FONT_COLOUR_1 = '#000000'
             self.FONT_COLOUR_2 = '#ffffff'
-        elif self.app_mode == 'dark':
+        elif self.APP_MODE == 'dark':
             self.BG_COLOUR = 'ffffff'  # change later for all
             self.HIGHLIGHT_COLOUR_1 = 'eeeeef'
             self.HIGHLIGHT_COLOUR_2 = 'cecece'
